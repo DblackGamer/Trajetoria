0c4ba1ba63ae7e1dcc528bb56c89318d945e7389
Juan <thesamecod3@gmail.com>
2024-11-12 20:28:08 -0300

    12-11-2024, For and While

---
+++ Looping.py
@@ -0,0 +1,11 @@
+#Aqui aprenderemos sobre o looping, utilizando o For e While
+#For serve para, geralmente printar tudo que há numa lista
+x= [1,2,3,4,5,6]
+for i in x:
+    print(i)
+#While tem uma função um pouco diferente
+
+y= 10
+while y <= 20 :
+    print (y)
+    y+=1
