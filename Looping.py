548d56120faaa69343c18ce38fa9b8d22b400b84
Juan <thesamecod3@gmail.com>
2024-11-12 22:10:08 -0300

    for, while.Alguns codigos mais complexos

--- Looping.py
+++ Looping.py
@@ -9,3 +9,34 @@ y= 10
 while y <= 20 :
     print (y)
     y+=1
+
+#Agora aqui é for com range
+for i in range(6):
+    print(f"Valor de `I` é:{i}")
+
+#Agora algo mais complexo
+
+# Exemplo de 'while' com condição de saída
+soma = 0
+numero = 1
+
+while soma < 10:
+    soma += numero  # adiciona o valor de 'numero' a 'soma'
+    print("Soma atual:", soma)
+    numero += 2  # aumenta o valor de 'numero' a cada iteração
+#For, tentar fazer aparecer um número primo
+for numero in range(1, 10):
+    if numero == 10:
+        break  # para o loop quando numero é 5
+    elif numero % 2 == 0:
+        continue  # pula números pares
+    print("Número ímpar:", numero)
+
+# Lista de dados de exemplo
+dados = [10, 20, 30]
+soma_total = 0
+
+for valor in dados:
+    soma_total = (soma_total+valor)/2
+
+print("Media total dos dados:", soma_total)
