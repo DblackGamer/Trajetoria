738ada6d0c7c8620cef3dbcd91b30ecde0224864
Juan <thesamecod3@gmail.com>
2025-05-03 18:49:09 -0300

    Aqui fiz conexão com banco de dados e teste de idade

--- Banco de Dados/conexão.py
+++ Banco de Dados/conexão.py
@@ -3,10 +3,51 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+  database="mydatabase"
 )
-
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
-print(mydb)
+#Aqui ele deleta a tabela convidados caso ela já exista.
+mycursor.execute("DROP TABLE IF EXISTS convidados")
+
+#Aqui ele cria a tabela convidados com os campos nome e idade.
+mycursor.execute("CREATE TABLE convidados (nome VARCHAR(255), idade int(11))")
+
+#Aqui mostra os convidados que estão na lista.
+def lista_convidados():
+    mycursor.execute("SELECT * FROM convidados")
+    for i in mycursor:
+        print(i)
+lista_convidados()
+
+#Aqui mostra os convidados que estão na lista.
+def lista_convidados():
+    mycursor.execute("SELECT * FROM convidados")
+    for i in mycursor:
+        print(i)
+lista_convidados()
+#Convidado
+nome_convidado=input("Digite seu nome:")
+idade_convidado=int(input("Digite sua idade:"))
+
+#aqui verifica e inclui na lista de convidados.
+def inclusão_na_lista():
+        if idade_convidado >= 18:
+            print("Você pode entrar")
+            mycursor.execute("INSERT INTO convidados (nome, idade) VALUES (%s, %s)", (nome_convidado, idade_convidado)) 
+            mydb.commit()
+            print(mycursor.rowcount, "registro(s) inserido(s).")
+            
+        else:
+            print("Você não pode entrar")
+inclusão_na_lista()
+
+#Aqui mostra os convidados que estão na lista.
+def lista_convidados():
+    mycursor.execute("SELECT * FROM convidados")
+    for i in mycursor:
+        print(i)
+lista_convidados()
+
+
