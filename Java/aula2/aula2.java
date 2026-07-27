a463ffe455c47b6f1549efff8ecdbe89f2a5f747
Juan <thesamecod3@gmail.com>
2026-07-27 18:40:14 -0300

    .

--- Java/aula2/aula2.java
+++ Java/aula2/aula2.java
@@ -1,4 +1,5 @@
 public class aula2 {
+    //Here we have de primitives data
     public static void main(String[] args) {
         String name = "Juan";
         int idade = 25;
@@ -7,6 +8,8 @@ public class aula2 {
         Boolean Verdadeiro = true;
         Boolean Falso= false;
         System.out.println("Meu nome é "+ name +", e tenho" + idade + " anos");
-
+        System.out.println("Eu sou homem");
+        System.out.println("A mensagem acima é " + Verdadeiro);
+        System.out.println(Falso);
     }
 }
---
+++ Java/aula3/aula3.java
@@ -0,0 +1,16 @@
+//Loop structure
+public class aula3 {
+    public static void main(String[] args) {
+       //Variable Declared
+        int total = 0;
+        //Struture
+        //for(Begin // type of variable -> variable -> = -> value;End//Variable -> Type of end//Until the ends not comes do something.)
+        //
+        for (int i = 0; i<=10;i++){
+            System
+        }
+        
+
+}
+
+    }
