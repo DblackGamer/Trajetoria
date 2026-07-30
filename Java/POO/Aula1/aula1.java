03fe1cdba65ee3b92bceeef109dff254de6a1ecb
Juan <thesamecod3@gmail.com>
2026-07-30 16:15:44 -0300

    .

---
+++ Java/POO/Aula1/aula1.java
@@ -0,0 +1,14 @@
+public class aula1 {
+//Here we created a object
+    public static void main(String[] args) {
+            class Aluno {
+            String nome;
+            int nota;
+        }
+        Aluno aluno1= new Aluno();
+        aluno1.nome= "Juan";
+        aluno1.nota= 6;
+
+        System.out.println(aluno1.nome);
+    }
+}
