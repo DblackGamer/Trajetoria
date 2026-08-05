9c1ca6046d85f3d7b76152a10a3a6da323df5e65
Juan <thesamecod3@gmail.com>
2026-08-04 22:09:25 -0300

    Trocar para Switch

--- Java/Exercicios/Exercicios 5/Exercicio5.java
+++ Java/Exercicios/Exercicios 5/Exercicio5.java
@@ -4,6 +4,7 @@ public class Exercicio5 {
         // Fazer um levantamento de quanto de taxa pagar de acordo com quant voce recebe
         Scanner input = new Scanner(System.in);
         int sair= 1 ;
+        
      
         do {
             System.out.println("Ola Mundo, para iniciarmos o calculo da taxa do salario siga as instrucoes abaixo");
@@ -13,13 +14,24 @@ public class Exercicio5 {
             System.out.println("3- Sair");
             // sair = 0;
             sair = input.nextInt();
-            if (sair == 1) {
+            
+
+       }  while (sair!= 3 && sair !=1);
+
+       if (sair == 1) {
                 System.out.println("Voce escolheu a opcao 1. Digite o seu salario.");
+                int salario = input.nextInt();
+                       System.out.println("Seu salario e R$" + salario + ",00");
+                              if (salario>=1500){
+                                int salario30 = salario + salario *30/100;
+                                System.out.println("Seu salario recebeu 30% de juros e ficou em: "+salario30);
+
+        
+       }
        }
 
-       }  while (sair!= 3);
+
        
 
     }
 }
- 
